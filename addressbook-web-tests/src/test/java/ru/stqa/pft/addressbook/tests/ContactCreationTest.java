2ccc32eea4ad2e9ae09d9d2432a43dab7c03ca92
AlexZltr <aklykova@gmail.com>
2021-07-21 01:10:22 +0300

    4.3 Количество элементов в списке контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.ContactData;
 
@@ -7,8 +8,11 @@ public class ContactCreationTest extends TestBase{
 
   @Test
   public void testContactCreation() throws Exception {
+    int before = app.getContactHelper().getContactCount();
     app.getNavigationHelper().gotoAddContactPage();
     app.getContactHelper().createContact(new ContactData("Василий", "Петров", "СПб, ул.Петрова, д.2", "222-33-44", "mail1@mail.ru", "test1"));
+    int after = app.getContactHelper().getContactCount();
+    Assert.assertEquals(after, before + 1);
     app.getSessionHelper().logout();
   }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTest.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
@@ -8,14 +9,18 @@ public class ContactModificationTest extends TestBase{
   @Test
   public void testContactModification() {
     app.getNavigationHelper().gotoHome();
+    int before = app.getContactHelper().getContactCount();
     if (!app.getContactHelper().isThereAContact()) {
       app.getNavigationHelper().gotoAddContactPage();
       app.getContactHelper().createContact(new ContactData("Василий", "Петров", "СПб, ул.Петрова, д.2", "222-33-44", "mail1@mail.ru", "test1"));
+      before = 1;
     }
     app.getContactHelper().selectContact();
     app.getContactHelper().initContactModification();
     app.getContactHelper().fillNewContact(new ContactData("ВасилийModification", "ПетровModification", "СПб, ул.Петрова, д.2", "222-33-44", "mail1@mail.ru", null), false);
     app.getContactHelper().submitContactModification();
     app.getNavigationHelper().gotoHome();
+    int after = app.getContactHelper().getContactCount();
+    Assert.assertEquals(after, before);
   }
 }
